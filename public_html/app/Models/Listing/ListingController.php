5c5d2df4f6c17bdd31a226aaad0407db4084cd2a
nopphasinara <nopphasin.arayasirikul@gmail.com>
2018-07-28 21:03:57 +0700

    Update source

--- public_html/app/Models/Listing/ListingController.php
+++ public_html/app/Models/Listing/ListingController.php
@@ -11,26 +11,26 @@ use App\Http\Controllers\Controller;
 
 class ListingController extends Controller
 {
-    
+
     public function show($id = '', $slug = '')
     {
       $property = Listing::byId($id);
       if (!$property) return abort(404);
-      
+
       return view('property-details', compact('property', 'id', 'slug'));
     }
-    
+
     public function search()
     {
       $orderBy = request('order', 'created_at');
       $sort = request('sort', 'desc');
-      
+
       if (strtolower($orderBy) == 'price') {
         $properties = Listing::orderBy('sale_price', $sort)->orderBy('rent_price', $sort);
       } else {
         $properties = Listing::orderBy($orderBy, $sort);
       }
-      
+
       $keywords = request('s', '');
       if ($keywords) {
         $properties->where('name', 'like', '%'.$keywords.'%')
@@ -38,23 +38,23 @@ class ListingController extends Controller
                    ->orWhere('refno', 'like', '%'.$keywords.'%')
                    ->orWhere('content', 'like', '%'.$keywords.'%');
       }
-      
+
       $propertyType = request('propertyType', '');
       if ($propertyType) {
         $properties->where('listing_type_id', $propertyType);
       }
-      
+
       $propertyFor = request('propertyFor', '');
       $propertyFor = strtolower($propertyFor);
       if ($propertyFor) {
         $properties->where('for_'. strtolower($propertyFor) .'', 1);
       }
-      
+
       $location = request('location', '');
       if ($location) {
         $properties->where('location_id', $location);
       }
-      
+
       $minBeds = request('minBeds', '');
       if ($minBeds) {
         $properties->where('bedrooms', '>=', $minBeds);
@@ -63,34 +63,34 @@ class ListingController extends Controller
       if ($maxBeds) {
         $properties->where('bedrooms', '<=', $maxBeds);
       }
-      
+
       $properties = $properties->where('web_visible', 1)->paginate();
-      
+
       $randomKeywords = '';
       if ($properties && $properties->count()) {
         $randomProperty = $properties->shuffle()->first();
         if ($randomProperty) $randomKeywords = $randomProperty->randomKeywords;
       }
-      
+
       return view('search', compact('properties'))->with([
         'keywords' => $keywords,
         'randomKeywords' => $randomKeywords,
       ]);
-      
+
       return view('search', compact('properties'))->with([
         'keywords' => $keywords,
       ]);
     }
-    
+
     public function byLocation($slug, $for = '', Request $request)
     {
       if (!$slug) return abort(404);
-      
+
       $keywords = $request->get('s', '');
-      
+
       $location = Location::where('web_visible', 1)->where('slug', $slug)->first();
       if (!$location) return abort(404);
-      
+
       $orderBy = request('order', 'created_at');
       $sort = request('sort', 'desc');
       if (strtolower($orderBy) == 'price') {
@@ -98,21 +98,58 @@ class ListingController extends Controller
       } else {
         $properties = Listing::where('web_visible', 1)->where('location_id', $location->id)->orderBy($orderBy, $sort);
       }
-      
+
       // $properties = $properties->where('location_id', $location->id);
       if ($for) {
         $for = strtolower($for);
         $properties = $properties->where('for_'. $for .'', 1);
       }
-      
+
       $properties = $properties->paginate();
-      
+
       $randomKeywords = '';
       if ($properties && $properties->count()) {
         $randomProperty = $properties->shuffle()->first();
         if ($randomProperty) $randomKeywords = $randomProperty->randomKeywords;
       }
-      
+
+      return view('search', compact('properties'))->with([
+        'keywords' => $keywords,
+        'randomKeywords' => $randomKeywords,
+      ]);
+    }
+
+    public function byCategory($slug = '')
+    {
+      if (!$slug) return abort(404);
+
+      $keywords = request('s', '');
+
+      $category = ListingType::where('slug', $slug)->where('web_visible', 1)->first();
+      if (!$category) return abort(404);
+
+      $for = request('for', '');
+      $orderBy = request('order', 'created_at');
+      $sort = request('sort', 'desc');
+      if (strtolower($orderBy) == 'price') {
+        $properties = Listing::where('web_visible', 1)->where('listing_type_id', $category->id)->orderBy('sale_price', $sort)->orderBy('rent_price', $sort);
+      } else {
+        $properties = Listing::where('web_visible', 1)->where('listing_type_id', $category->id)->orderBy($orderBy, $sort);
+      }
+
+      if ($for) {
+        $for = strtolower($for);
+        $properties = $properties->where('for_'. $for .'', 1);
+      }
+
+      $properties = $properties->paginate();
+
+      $randomKeywords = '';
+      if ($properties && $properties->count()) {
+        $randomProperty = $properties->shuffle()->first();
+        if ($randomProperty) $randomKeywords = $randomProperty->randomKeywords;
+      }
+
       return view('search', compact('properties'))->with([
         'keywords' => $keywords,
         'randomKeywords' => $randomKeywords,
