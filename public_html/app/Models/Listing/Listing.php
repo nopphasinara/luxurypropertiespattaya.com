9eb8fdedbb463ce9ad88a4f7f96363bdbe94546d
nopphasinara <nopphasin.arayasirikul@gmail.com>
2018-07-30 15:55:43 +0700

    Update source

--- public_html/app/Models/Listing/Listing.php
+++ public_html/app/Models/Listing/Listing.php
@@ -13,25 +13,25 @@ use Illuminate\Support\Facades\Storage;
 
 class Listing extends Model
 {
-  
+
   protected $table = 'listings';
   protected $fillable = [];
   protected $perPage = 12;
-  
+
   public static function byId($id)
   {
     return Listing::where('id', $id)->first();
   }
-  
+
   public static function bySlug($slug)
   {
     return Listing::where('slug', $slug)->first();
   }
-  
+
   public function randomKeywords($total = 10)
   {
     $keywords = [];
-    
+
     if ($this->keywords) {
       $keywords = str_replace([
         ' , ',
@@ -41,25 +41,25 @@ class Listing extends Model
       $keywords = explode(',', $keywords, $total + 1);
       if (isset($keywords[$total])) unset($keywords[$total]);
     }
-    
+
     $keywords = implode(', ', $keywords);
-    
+
     return $keywords;
   }
-  
+
   public function getRandomKeywordsAttribute()
   {
     return $this->randomKeywords();
   }
-  
+
   public function getBedroomsAttribute() {
     return ($this->attributes['bedrooms']) ? $this->attributes['bedrooms'] : '';
   }
-  
+
   public function getBathroomsAttribute() {
     return ($this->attributes['bathrooms']) ? $this->attributes['bathrooms'] : '';
   }
-  
+
   public function getOwnershipAttribute() {
     if ($this->attributes['ownership']) {
       $ownership_id = $this->attributes['ownership'];
@@ -67,48 +67,48 @@ class Listing extends Model
       if ($ownership) {
         $ownership->value = json_decode($ownership->value, true);
       }
-      
+
       return title_case($ownership->value[$ownership_id]);
     }
   }
-  
+
   public function getFeaturesListAttribute()
   {
     $result = '';
     $value = json_decode($this->attributes['features'], true);
-    
+
     if ($value) {
       $data = Features::whereIn('id', $value)->get();
       if ($data && $data->count()) {
         $result = $data;
       }
     }
-    
+
     return $result;
   }
-  
+
   public function getFacilitiesListAttribute()
   {
     $result = '';
     $value = json_decode($this->attributes['facilities'], true);
-    
+
     if ($value) {
       $data = Facilities::whereIn('id', $value)->get();
       if ($data && $data->count()) {
         $result = $data;
       }
     }
-    
+
     return $result;
   }
-  
+
   public function getThumbnailAttribute()
   {
-    $file = storage_exists('uploads', $this->attributes['image']);
+    $file = storage_exists('uploads',  'thumbnails/' . $this->attributes['image']);
     if (!$file) return placeholder();
     return $file['url'];
   }
-  
+
   public function getGalleryAttribute($value)
   {
     if (substr($value, -3) == 'jpg') {
@@ -116,7 +116,7 @@ class Listing extends Model
     }
     return json_decode($this->attributes['gallery'], true);
   }
-  
+
   public function getVideoIdAttribute()
   {
     $url = $this->attributes['video'];
@@ -124,7 +124,7 @@ class Listing extends Model
     $host = (isset($parse['host'])) ? strtolower($parse['host']) : '';
     $query = [];
     $video_id = '';
-    
+
     if (isset($parse['query'])) {
       $exp = explode('&', $parse['query']);
       foreach ($exp as $index => $value) {
@@ -132,26 +132,26 @@ class Listing extends Model
         $query[$name] = $val;
       }
     }
-    
+
     if (in_array($host, ['youtube.com', 'www.youtube.com'])) {
       $video_id = (isset($query['v'])) ? $query['v'] : '';
     } elseif (in_array($host, ['youtu.be', 'www.youtu.be'])) {
       $video_id = (isset($parse['path'])) ? trim($parse['path'], '/') : '';
     }
-    
+
     return $video_id;
   }
-  
+
   public function getSalePriceAttribute()
   {
     return number_format($this->attributes['sale_price'], 0, '.', ',');
   }
-  
+
   public function getRentPriceAttribute()
   {
     return number_format($this->attributes['rent_price'], 0, '.', ',');
   }
-  
+
   public function getPermalinkAttribute()
   {
     if (!$this->attributes['slug']) {
@@ -159,27 +159,27 @@ class Listing extends Model
     }
     return url('property-details/' . $this->attributes['id'] . '/' . $this->attributes['slug']);
   }
-  
+
   public function getListingTypeAttribute()
   {
     return ListingType::where('id', $this->attributes['listing_type_id'])->first();
   }
-  
+
   public function getLocationAttribute()
   {
     return Location::where('id', $this->attributes['location_id'])->first();
   }
-  
+
   public function scopeFeatured($query)
   {
     return $query->visible()->where('featured', 1);
   }
-  
+
   public function scopeVisible($query)
   {
     return $query->where('web_visible', 1)->orderBy('id', 'desc');
   }
-  
+
   public function scopeVisibleNoOrder($query)
   {
     return $query->where('web_visible', 1);
